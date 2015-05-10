6ed92f28212b308d3a5d0b61176b1b820641cb12
AnaBendovska <anna95bb@gmail.com>
2015-05-10 03:40:11 +0200

    first commit

---
+++ DeepSpaceBattles/Home.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace DeepSpaceBattles
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+            SoundPlayer sound = new SoundPlayer("C:\\Users\\Ana\\Downloads\\DeepSpaceBattles(2)\\DeepSpaceBattles\\DeepSpaceBattles\\Resources\\Star_Wars_Soundtrack.wav");
+            sound.PlayLooping();
+            Image img = DeepSpaceBattles.Properties.Resources.redfighter0005;
+            pictureBox1.Image = img;
+        }
+
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure that you want to quit?", "Quit?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            Form1 f = new Form1();
+            f.ShowDialog();
+        }
+
+        private void btnHowToPlay_Click(object sender, EventArgs e)
+        {
+            HowToPlay h = new HowToPlay();
+            h.ShowDialog();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ DeepSpaceBattles/HowToPlay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace DeepSpaceBattles
+{
+    public partial class HowToPlay : Form
+    {
+        public HowToPlay()
+        {
+            InitializeComponent();
+            SoundPlayer sound = new SoundPlayer("C:\\Users\\Ana\\Downloads\\DeepSpaceBattles(2)\\DeepSpaceBattles\\DeepSpaceBattles\\Resources\\Star_Wars_Soundtrack.wav");
+            sound.PlayLooping();
+            Image img = DeepSpaceBattles.Properties.Resources.Untitled;
+            pictureBox1.Image = img;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
